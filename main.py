73ab2cd5d77438d92eb6b382fcfa57baeb9a7695
JoelO <joel.oranen@gmail.com>
2024-02-08 15:40:02 +0200

    updated the quiz, should work

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ quiz = {
     "What is the color of the sky on a clear day?": "Blue",
     "What is the chemical symbol for water?": "H2O"
 }
-correct_answers_count = 0
-for question, answer in quiz.items():
-    user_answer = input(question + " ")
-    if user_answer.lower() == answer.lower():
-        print("Correct!")
-        correct_answers_count += 1
-    else:
-        print("Incorrect. The correct answer is:", answer)
+def kysely(quiz):
+    correct_answers_count = 0
+    for question, answer in quiz.items():
+        user_answer = input(question + " ")
+        if user_answer.lower() == answer.lower():
+            print("Correct!")
+            correct_answers_count += 1
+        else:
+            print("Incorrect. The correct answer is:", answer)
+    return correct_answers_count
 
 def start():
     while True:
@@ -20,7 +22,7 @@ def start():
         if play.lower() == "y":
             print("Yay, let's play!")
             # Show questions here and count the score
-            score = 10 #change this
+            score = kysely(quiz) #change this
             display_score(score)
             break
         elif play.lower() == "n":
